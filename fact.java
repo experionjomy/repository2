3db182e8c90a2113f769710b9edee79432d84d91
experionjomy <jomyjose3009@gmail.com>
2016-10-27 14:48:50 +0530

    new file

---
+++ fact.java
@@ -0,0 +1,20 @@
+class fact
+{
+	public static void main(String args[])
+	{
+ 		System.out.println("Enter the numSber");
+		BufferReader br=new BufferedReader(new 					InputStreamReader(System.in));
+		int a=interger.pasrseInt(br.readLine);
+		int s=1;
+		for(i=a;i>1;i--)
+		{
+			
+ 			int s=s*i;
+			
+		}
+		
+		System.out.println(s);
+
+
+	}
+}
